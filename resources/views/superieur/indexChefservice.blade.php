972ccf5d5df1c0c15fb587792343ea1302ca17c0
elsacarrena <elsacarrena@gmail.com>
2024-05-29 11:32:40 +0100

    n

--- resources/views/superieur/indexChefservice.blade.php
+++ resources/views/superieur/indexChefservice.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.chefservice')
+@extends('layouts.superieur')
 
 @section('content')
 <div class="container">
@@ -9,7 +9,7 @@
 
             <div class="card-body">
                 <link href="{{ asset('css/index.css') }}" rel="stylesheet">
-                <a href="{{ route('chef_service.create') }}" class="btn btn-primary">Ajouter un chef de service</a>
+                <a href="{{ route('superieur.ChefServiceFormulaire') }}" class="btn btn-primary">Ajouter un chef de service</a>
 
                 @if (session('success'))
                     <div class="alert alert-success">
@@ -23,10 +23,10 @@
                             <th>id</th>
                             <th>Nom</th>
                             <th>numero</th>
-                            <th>Date de prise de service</th>
-                            <th>Date de fin de service</th>
-                            <th>Date additionnelle</th>
                             <th>domaine</th>
+                            <th>date_debut</th>
+                            <th>date_fin</th>
+                            <th>date_additionnelle</th>
                              <th>localisation</th>
                              <th>Numéro urgence</th>
                             <th>Actions</th>
@@ -40,17 +40,20 @@
                                 <td>{{ $chefservices->numero}}</td>
                                 <td>{{ $chefservices->domaine}}</td>
                                 <td>{{ $chefservices->date_debut}}</td>
-                                <td>{{ $chefservices->date_fin}}</td>
+                                <td>{{ $chefservices-> date_fin}}</td>
                                 <td>{{ $chefservices->date_additionnelle}}</td>
                                  <td>{{ $chefservices->localisation}}</td>
                                  <td>{{ $chefservices->numero_urgence }}</td>
                                 <td>
-                                    <a href="{{ route('chef_service.edit', $chefservices) }}" class="btn btn-primary btn-sm">Modifier un  chef de service</a>
-                                    <form action="{{route('chef_service.destroy', $chefservices ->id) }}" method="POST" style="display: inline;" onsubmit="return confirmDelete()">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-sm btn-danger">Supprimer un chef de service</button>
-                                    </form>
+                                    <a href="{{ route('superieur.editChefservice', $chefservices, ['id'=>$chefservices->id])}}" class="btn btn-primary btn-sm">Modifier un  chef de service</a>
+                                     {{--  <form action="{{route('chef_service.destroy', ['id'=>$chefservices->id]) }}" method="POST" style="display: inline;" onsubmit="return confirmDelete()">  --}}
+                                        <form action="{{ route('superieur.destroyChefservice', $chefservices->id) }}" method="POST" style="display: inline;" onsubmit="return confirmDelete()">
+
+                                          @csrf
+                                          @method('DELETE')
+                                          <button type="submit" class="btn btn-sm btn-danger">Supprimer un chef de service</button>
+
+                                        </form>
                                 </td>
                             </tr>
                         @endforeach
